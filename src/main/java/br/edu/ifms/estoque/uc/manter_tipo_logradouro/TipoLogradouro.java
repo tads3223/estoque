e55a69260b88ecdb01ba0cfa4cdaea4d6e76f1e0
Nicholas Eduardo Lopes dos Santos <nicholas.santos@ifms.edu.br>
2025-04-08 23:45:26 -0300

    criação do tipo de logradouro

---
+++ src/main/java/br/edu/ifms/estoque/uc/manter_tipo_logradouro/TipoLogradouro.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifms.estoque.uc.manter_tipo_logradouro;
+
+/**
+ *
+ * @author 1513003
+ */
+public class TipoLogradouro {
+    private Long id;
+    private String nome;
+    
+    public TipoLogradouro() {}
+
+    public TipoLogradouro(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
---
+++ src/main/java/br/edu/ifms/estoque/uc/manter_tipo_logradouro/TipoLogradouroController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifms.estoque.uc.manter_tipo_logradouro;
+
+import java.util.LinkedList;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author 1513003
+ */
+@RestController
+public class TipoLogradouroController {
+    
+    private List<TipoLogradouro> lista = new LinkedList<>();
+    
+    @RequestMapping(method = RequestMethod.POST, name = "/tipo-logradouro")
+    public TipoLogradouro create(
+            Long id,
+            String nome
+    ) {
+        TipoLogradouro tipoLogradouro = new TipoLogradouro(id, nome);
+        lista.add(tipoLogradouro);
+        return tipoLogradouro;
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, name = "/tipo-logradouro")
+    public List<TipoLogradouro> list() {
+        return lista;
+    }
+    
+}
